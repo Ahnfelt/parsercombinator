4951e0c5acd2beafe19a578ee6dc2aabbef24250
Joakim Ahnfelt-Rønne <joakim.ahnfelt@gmail.com>
2015-03-03 06:59:44 +0100

    Moved some stuff out of the parseIf method

--- src/main/java/dk/ahnfelt/parsercombinator/Main.java
+++ src/main/java/dk/ahnfelt/parsercombinator/Main.java
@@ -15,27 +15,28 @@ public class Main {
 
         String test = "if a then b elseif c then d elseif c2 then d2 else e end";
 
-        Parser<String> token = regex("([a-z][a-z0-9]*)(\\s+|$)").map(m -> m.group(1));
-        Parser<String> variable = token.filter(t -> !Arrays.asList("if", "then", "else", "elseif", "end").contains(t));
-        Function<String, Parser<String>> keyword = k -> token.filter(k::equals);
-
         Parser<Pair<String, String>> parseElseIf =
-                skip(keyword.apply("elseif")).
+                skip(keyword("elseif")).
                 then(variable).
-                skip(keyword.apply("then")).
+                skip(keyword("then")).
                 then(variable);
 
         Parser<String> parseIf =
-                skip(keyword.apply("if")).then(variable).
-                skip(keyword.apply("then")).then(variable).
+                skip(keyword("if")).then(variable).
+                skip(keyword("then")).then(variable).
                 then(parseElseIf.zeroOrMore()).
-                then(skip(keyword.apply("else")).then(variable).optional()).
-                skip(keyword.apply("end")).
+                then(skip(keyword("else")).then(variable).optional()).
+                skip(keyword("end")).
                 skip(end()).
                 map(match((condition, then, elseIfs, otherwise) -> elseIfs.toString()));
 
         System.out.println(parseIf.parse(test));
 
     }
+
+    private static Parser<String> token = regex("([a-z][a-z0-9]*)(\\s+|$)").map(m -> m.group(1));
+    private static Parser<String> variable = token.filter(t -> !Arrays.asList("if", "then", "else", "elseif", "end").contains(t));
+    private static Parser<String> keyword(String k) { return token.filter(k::equals); }
+
 
 }
